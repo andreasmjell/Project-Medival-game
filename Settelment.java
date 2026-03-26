784d184b84bd4456f5da41254c2bcc61e635ae75
andreasmjell <andreas.mjell@gmail.com>
2026-03-26 15:13:05 +0100

    kjør

---
+++ Settelment.java
@@ -0,0 +1,16 @@
+public class Settelment {
+    String navn;
+    int x;
+    int y;
+    int troops;
+    int timer;
+
+    public Settelment(String navn, int x, int y, int troops, int timer){
+        
+        this.x = x;
+        this.y = y;
+        this.troops = troops;
+        this.timer = timer;
+
+    }
+}
