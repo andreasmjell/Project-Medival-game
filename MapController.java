62a4329b4aeb87ad63eb7dfc8f887c0394515d30
andreasmjell <andreas.mjell@gmail.com>
2026-03-26 14:46:37 +0100

    Plan oppdatering

--- MapController.java
+++ MapController.java
@@ -1,3 +1,43 @@
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+import org.w3c.dom.events.MouseEvent;
+
 public class MapController {
-    
+   public class SmoothMoveCharacter extends JPanel {
+    private int charX = 50; // startposisjon
+    private int charY = 50;
+    private final int CHAR_SIZE = 20;
+
+    private int targetX = charX;
+    private int targetY = charY;
+    private final int SPEED = 5; // hvor mange piksler karakteren beveger seg per steg
+
+    private Image mapImage = new ImageIcon("map.png").getImage();
+    private Image charImage = new ImageIcon("ridderhjelm.png").getImage();
+
+    public SmoothMoveCharacter() {
+        // Lytter til museklikk
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                targetX = e.getX() - CHAR_SIZE / 2;
+                targetY = e.getY() - CHAR_SIZE / 2;
+            }
+        });
+
+        // Timer som oppdaterer posisjon hvert 20 ms (~50 FPS)
+        Timer timer = new Timer(20, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                moveCharacter();
+                repaint();
+            }
+        });
+        timer.start();
+
+        //this.setPreferredSize(new Dimension(1920, 1080));
+        this.setPreferredSize(new Dimension(mapImage.getWidth(null), mapImage.getHeight(null)));
+    }
+
 }
