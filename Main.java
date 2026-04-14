d245e6254f0ec570bd27ccf9c7ec87d9eb83d861
andreasmjell <andreas.mjell@gmail.com>
2026-04-14 17:24:06 +0200

    Se main

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+/* for å starte spillet med JSON:
+javac -cp ".;lib/json-20251224.jar" *.java
+java -cp ".;lib/json-20251224.jar" .\Main.java
+*/
+
 public class Main {
     public static void main(String[] args) {
         MapController mc = new MapController();
