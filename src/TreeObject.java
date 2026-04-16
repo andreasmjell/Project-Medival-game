e8b88dd75e2e44cb9e2c23a58d4afe161f2ec8fb
andreasmjell <andreas.mjell@gmail.com>
2026-04-16 17:05:08 +0200

    asd

--- src/TreeObject.java
+++ src/TreeObject.java
@@ -29,10 +29,6 @@ public class TreeObject implements Drawable{
         int px = (int)(x - cameraX - 400);
         int py = (int)(y - cameraY -400);
 
-<<<<<<< HEAD
-        g.drawImage(img, px, py, 80, 80, null);
-=======
         g.drawImage(treeImage, px, py, 800, 800, null);
->>>>>>> 843c635f98c1062fc344cc0fe155ddddf7fc008f
     }
 }
