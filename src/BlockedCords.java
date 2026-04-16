35e32e52f88669b127d91865dca6d9518f972536
andreasmjell <andreas.mjell@gmail.com>
2026-04-16 13:58:41 +0200

    lagt til blocked

---
+++ src/BlockedCords.java
@@ -0,0 +1,49 @@
+package src;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class BlockedCords {
+
+    private boolean[][] blocked;
+    private int width;
+    private int height;
+
+    public void loadBlockedMap() {
+        try {
+            BufferedImage img = ImageIO.read(this.getClass().getResourceAsStream("assets/mapBlocked.png"));
+
+            width = img.getWidth();
+            height = img.getHeight();
+
+            blocked = new boolean[width][height];
+
+            int blockedColor = new Color(237, 28, 36).getRGB();
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+
+                    if (img.getRGB(x, y) == blockedColor) {
+                        blocked[x][y] = true;
+                    }
+                    else{
+                        blocked[x][y] = false;
+                    }
+                }
+            }
+
+            System.out.println("Blocked map lastet!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isBlocked(int x, int y) {
+    if (x < 0 || x >= width || y < 0 || y >= height) {
+        return true; // Treat "out of bounds" as blocked
+    }
+    return blocked[x][y];
+    }
+}
