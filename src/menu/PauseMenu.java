3df8abfbc2163afb7606b5bb6bb8c07ef21f4f06
havardolmheim <havard.g.olmheim@gmail.com>
2026-04-17 13:33:20 +0200

    PauseMenu

--- src/menu/PauseMenu.java
+++ src/menu/PauseMenu.java
@@ -12,29 +12,35 @@ public class PauseMenu extends GameMenu{
     public PauseMenu(MapController mapController){
         mapController.timerStop();
         uiHandler = mapController.getUiHandler();
+
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-        int menuWidth = (int) (size.getWidth());
-        int menuHeight = (int) (size.getHeight());
+
+        int menuWidth = (int)(size.getWidth() * 0.2);
+        int menuHeight = (int)(size.getHeight() * 0.6);
 
         int x = ((int)size.getWidth() - menuWidth) / 2;
         int y = ((int)size.getHeight() - menuHeight) / 2;
 
-        super(x,y,menuWidth,menuHeight);
+        super(x, y, menuWidth, menuHeight);
+
         JLabel bg = new JLabel(new ImageIcon(getClass().getResource("/src/assets/Mainmenu.png")));
-        int backgroundWidth = (int) (size.getWidth() * 0.8);
-        int backgroundHeight = (int) (size.getHeight() * 0.8);
-        bg.setBounds(0, 0, backgroundWidth, backgroundHeight);
+        bg.setBounds(0, 0, menuWidth, menuHeight);
 
-        
+        MenuButton closeBtn = new MenuButton("Close Menu");
 
+        int btnWidth = 250;
+        int btnHeight = 60;
+        int btnX = (menuWidth - btnWidth) / 2;
+        int btnY = (int)(menuHeight * 0.75);
+        closeBtn.setFont(new Font("Serif", Font.BOLD, 24));
 
-        // Eksempel på lukk-knapp nederst i midten av menyen
-        MenuButton closeBtn = new MenuButton("Forlat byen");
-        closeBtn.setBounds((menuWidth - 250) / 2, menuHeight - 80, 250, 60);
+        closeBtn.setBounds(btnX, btnY, btnWidth, btnHeight);
+
         closeBtn.addActionListener(e -> {
             mapController.timerStart();
             uiHandler.closeMenu(this);
         });
+
         this.add(closeBtn);
         this.add(bg);
     }
